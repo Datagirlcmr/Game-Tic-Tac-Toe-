75b5ac40c355a6b681d80184b5a8137c82f6e732
Selma Datagirl <datagirlcmr@gmail.com>
2020-02-27 11:29:34 -0600

    enumerate the gameflow objects

---
+++ main.js
@@ -0,0 +1,15 @@
+const Player = (name, symbol) => {
+   return {name, symbol};
+};
+
+const DisplayBoard = (() => {
+    const gameBoard = [0,1,2,3,4,5,6,7,8];
+})();
+
+const DisplayController = (() => {
+
+})();
+
+const GameFlow = () => {
+
+};
