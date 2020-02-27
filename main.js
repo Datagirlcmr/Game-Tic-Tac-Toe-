b618f50bcd791361fffb9c28876739e2783d15a2
Edemone <edem.agbenyo@gmail.com>
2020-02-27 22:01:23 +0000

    Implement adding of marks to position and check of game status(win & tie)

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const Player = (name, symbol) => {
-   return {name, symbol};
+   const playing = false;
+    return {name, symbol,playing};
 };
 
 const DisplayBoard = (() => {
-    const gameBoard = ['X','','','','','','','','X',];
+    const gameBoard = ['','','','','','','','','',];
 
     return {gameBoard}
 })();
@@ -11,19 +12,98 @@ const DisplayBoard = (() => {
 const DisplayController = (() => {
 
     const boxes = document.querySelectorAll('td');
-    const initialize = ()=>{
-        const board = DisplayBoard;
-        boxes.forEach(box=>{
-            const id = box.dataset.id;
-            box.innerHTML = board.gameBoard[id];
-        })
+    let boardCount =0;
+    let status = ""
+    const addMark =  (event)=>{
+        const {index} = event.target.dataset;
+        
+        if(GameFlow.checkSpace(index) && status !== "win" && status !=="tie"){
+            const currentPlayer = GameFlow.playerTurn()
+            event.target.innerHTML = currentPlayer.symbol
+
+            //update the game board array
+            GameFlow.updateBoard(currentPlayer.symbol, index)
+            status= GameFlow.gameStatus(currentPlayer.symbol, boardCount)
+            boardCount++
+
+            console.log(status);
+        }else{
+            console.log(status);
+        }
+        
     }
+    const initialize = (board)=>{
+        boxes.forEach(box=>{
+            const id = box.dataset.index;
+            box.innerHTML = board.gameBoard[id];
+
+            //
+            box.addEventListener('click',addMark)
+        });
+    }
+
+    
+
 
     return {initialize}
 })();
 
-const GameFlow = () => {
+const p1 = Player('Selma','X')
+const p2 = Player('Edem','O')
 
-};
+const GameFlow = ((p1,p2) => {
+    const board = DisplayBoard;
+    
+    const start = ()=>{
+        
+        //Display the board with an empty array
+        DisplayController.initialize(board)
+        //others stuff
+    }
+    const checkSpace = (position)=>{
+        if(board.gameBoard[position]!==''){
+            return false
+        }else{
+            return true;
+        }
+    }
+    const playerTurn = ()=>{
+         if(!p1.playing){
+             p1.playing=true
+             p2.playing=false
+             return p1;
+         }else{
+             p1.playing=false
+             p2.playing=true
+             return p2;
+         }
+    }
 
-DisplayController.initialize()
+    const updateBoard = (mark,position)=>{
+        board.gameBoard[position] = mark
+    }
+
+    const gameStatus = (mark, boardCount)=>{
+        if(boardCount==board.gameBoard.length-1){
+            return "tie"
+        }
+        else if((board.gameBoard[0]==mark && board.gameBoard[1]==mark && board.gameBoard[2]==mark) 
+            || (board.gameBoard[3]==mark && board.gameBoard[4]==mark && board.gameBoard[5]==mark) 
+            || (board.gameBoard[6]==mark && board.gameBoard[7]==mark && board.gameBoard[8]==mark) 
+            || (board.gameBoard[0]==mark && board.gameBoard[3]==mark && board.gameBoard[6]==mark) 
+            || (board.gameBoard[1]==mark && board.gameBoard[4]==mark && board.gameBoard[7]==mark) 
+            || (board.gameBoard[2]==mark && board.gameBoard[5]==mark && board.gameBoard[8]==mark) 
+            || (board.gameBoard[0]==mark && board.gameBoard[4]==mark && board.gameBoard[8]==mark) 
+            || (board.gameBoard[2]==mark && board.gameBoard[4]==mark && board.gameBoard[6]==mark) 
+        ){
+            return "win"
+        }else{
+            return "continue"
+        }
+    }
+    return {start,checkSpace,playerTurn,gameStatus, updateBoard}
+})(p1,p2);
+
+
+GameFlow.start()
+
