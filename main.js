cde146c16f9d67bf5356937d5be1c3a5f35bcb37
Selma Datagirl <datagirlcmr@gmail.com>
2020-03-04 13:21:58 -0600

    commented out unused variables, formated main.js

--- main.js
+++ main.js
@@ -1,167 +1,181 @@
-const player1 = document.querySelector('#player1');
-const player2 = document.querySelector('#player2');
-const congrats = document.querySelector('#congrats');
-const pturn = document.querySelector('#player-turn');
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const congrats = document.querySelector("#congrats");
+// const pturn = document.querySelector("#player-turn");
 const form = document.querySelector("form");
-const boxes = document.querySelectorAll('td');
+const boxes = document.querySelectorAll("td");
 
 const Player = (name, symbol) => {
-   const playing = false;
-    return {name, symbol, playing};
+  const playing = false;
+  return { name, symbol, playing };
 };
 
 const DisplayBoard = (() => {
-    let gameBoard = ['','','','','','','','','',];
-    return {gameBoard}
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  return { gameBoard };
 })();
 
 const DisplayController = (() => {
+  const addMark = event => {
+    const { index } = event.target.dataset;
 
-
-    const addMark =  (event)=>{
-        const {index} = event.target.dataset;
-        
-        
-        if( GameFlow.checkSpace(index) && !GameFlow.gameEnd()){
-            const currentPlayer = GameFlow.playerTurn()
-            GameFlow.play(currentPlayer, index)
-            event.target.innerHTML = currentPlayer.symbol
-            if(GameFlow.gameWin(currentPlayer.symbol)){
-                congrats.innerHTML = `Hurray!! ${currentPlayer.name} won the game`
-            }
-            if(GameFlow.boardFull() ){
-
-                congrats.innerHTML = `Draw game`
-            }
-        }
-
+    if (GameFlow.checkSpace(index) && !GameFlow.gameEnd()) {
+      const currentPlayer = GameFlow.playerTurn();
+      GameFlow.play(currentPlayer, index);
+      event.target.innerHTML = currentPlayer.symbol;
+      if (GameFlow.gameWin(currentPlayer.symbol)) {
+        congrats.innerHTML = `Hurray!! ${currentPlayer.name} won the game`;
+      }
+      if (GameFlow.boardFull()) {
+        congrats.innerHTML = `Draw game`;
+      }
     }
-    const initialize = (board)=>{
-        boxes.forEach(box=>{
-            const id = box.dataset.index;
-            box.innerHTML = board.gameBoard[id];
-            
-            box.addEventListener('click',addMark)
-        });
-    }
+  };
+  const initialize = board => {
+    boxes.forEach(box => {
+      const id = box.dataset.index;
+      box.innerHTML = board.gameBoard[id];
 
-    const clearBoard = ()=>{
-       boxes.forEach(box=>{
-        box.innerHTML = ''
+      box.addEventListener("click", addMark);
+    });
+  };
 
-       });
-    };
- 
+  const clearBoard = () => {
+    boxes.forEach(box => {
+      box.innerHTML = "";
+    });
+  };
 
-    return {initialize, clearBoard}
+  return { initialize, clearBoard };
 })();
-
 
 const GameFlow = (() => {
-    const board = DisplayBoard;
-    let p1, p2;
-    let boardCount = 0;
-    let gameStatus = "playing"
-    let currentPlayer = null;
-    let nextPlayer = null;
-    const start = ()=>{
-         p1 =Player(player1.value, 'X')
-         p2 = Player(player2.value, 'O')
-        
-        //Display the board with an empty array
-        DisplayController.initialize(board)
-        //others stuff
-        
-    }
-    const checkSpace = (position)=>{
-        if(board.gameBoard[position]!==''){
-            return false
-        }else{
-            return true;
-        }
-    }
-    const playerTurn = ()=>{
-         if(!p1.playing){
-             p1.playing=true
-             p2.playing=false
-             currentPlayer=p1;
-             nextPlayer = p2;
-             return p1;
-            }else{
-                p1.playing=false
-                p2.playing=true
-                currentPlayer=p2;
-                nextPlayer = p1;
-             return p2;
-         }
-    }
-    
+  const board = DisplayBoard;
+  let p1, p2;
+  let boardCount = 0;
+  let gameStatus = "playing";
+//   let currentPlayer = null;
+  let nextPlayer = null;
+  const start = () => {
+    p1 = Player(player1.value, "X");
+    p2 = Player(player2.value, "O");
 
-    const changeGameStatus = ()=>{
-        gameStatus = "playing"
-        boardCount = 0
+    //Display the board with an empty array
+    DisplayController.initialize(board);
+    //others stuff
+  };
+  const checkSpace = position => {
+    if (board.gameBoard[position] !== "") {
+      return false;
+    } else {
+      return true;
     }
-    const play = (player,position)=>{
-            updateBoard(player.symbol, position)
-            if(gameWin(player.symbol)){
-                gameStatus = 'end'
-                return false
-            }
-            return true
+  };
+  const playerTurn = () => {
+    if (!p1.playing) {
+      p1.playing = true;
+      p2.playing = false;
+    //   currentPlayer = p1;
+      nextPlayer = p2;
+      return p1;
+    } else {
+      p1.playing = false;
+      p2.playing = true;
+    //   currentPlayer = p2;
+      nextPlayer = p1;
+      return p2;
     }
+  };
 
-    const updateBoard = (mark,position)=>{
-        board.gameBoard[position] = mark
-        boardCount++
-
+  const changeGameStatus = () => {
+    gameStatus = "playing";
+    boardCount = 0;
+  };
+  const play = (player, position) => {
+    updateBoard(player.symbol, position);
+    if (gameWin(player.symbol)) {
+      gameStatus = "end";
+      return false;
     }
+    return true;
+  };
 
-    const gameWin = (mark)=>{
-        if((board.gameBoard[0]==mark && board.gameBoard[1]==mark && board.gameBoard[2]==mark) 
-            || (board.gameBoard[3]==mark && board.gameBoard[4]==mark && board.gameBoard[5]==mark) 
-            || (board.gameBoard[6]==mark && board.gameBoard[7]==mark && board.gameBoard[8]==mark) 
-            || (board.gameBoard[0]==mark && board.gameBoard[3]==mark && board.gameBoard[6]==mark) 
-            || (board.gameBoard[1]==mark && board.gameBoard[4]==mark && board.gameBoard[7]==mark) 
-            || (board.gameBoard[2]==mark && board.gameBoard[5]==mark && board.gameBoard[8]==mark) 
-            || (board.gameBoard[0]==mark && board.gameBoard[4]==mark && board.gameBoard[8]==mark) 
-            || (board.gameBoard[2]==mark && board.gameBoard[4]==mark && board.gameBoard[6]==mark) 
-        ){
-            return true
-        }else{
-            return false
-        }
+  const updateBoard = (mark, position) => {
+    board.gameBoard[position] = mark;
+    boardCount++;
+  };
+
+  const gameWin = mark => {
+    if (
+      (board.gameBoard[0] == mark &&
+        board.gameBoard[1] == mark &&
+        board.gameBoard[2] == mark) ||
+      (board.gameBoard[3] == mark &&
+        board.gameBoard[4] == mark &&
+        board.gameBoard[5] == mark) ||
+      (board.gameBoard[6] == mark &&
+        board.gameBoard[7] == mark &&
+        board.gameBoard[8] == mark) ||
+      (board.gameBoard[0] == mark &&
+        board.gameBoard[3] == mark &&
+        board.gameBoard[6] == mark) ||
+      (board.gameBoard[1] == mark &&
+        board.gameBoard[4] == mark &&
+        board.gameBoard[7] == mark) ||
+      (board.gameBoard[2] == mark &&
+        board.gameBoard[5] == mark &&
+        board.gameBoard[8] == mark) ||
+      (board.gameBoard[0] == mark &&
+        board.gameBoard[4] == mark &&
+        board.gameBoard[8] == mark) ||
+      (board.gameBoard[2] == mark &&
+        board.gameBoard[4] == mark &&
+        board.gameBoard[6] == mark)
+    ) {
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    const gameEnd = ()=>{
-        if(boardCount==board.gameBoard.length || gameStatus=='end' ){
-            return true;
-        }else{
-            return false
-        }
+  const gameEnd = () => {
+    if (boardCount == board.gameBoard.length || gameStatus == "end") {
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    const boardFull = ()=>{
-        if(boardCount==board.gameBoard.length){
-            return true;
-        }else{
-            return false
-        }
+  const boardFull = () => {
+    if (boardCount == board.gameBoard.length) {
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    
-    return {start,checkSpace,playerTurn,gameEnd, updateBoard, gameWin,play,changeGameStatus, nextPlayer,boardFull}
+  return {
+    start,
+    checkSpace,
+    playerTurn,
+    gameEnd,
+    updateBoard,
+    gameWin,
+    play,
+    changeGameStatus,
+    nextPlayer,
+    boardFull
+  };
 })();
 
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  congrats.innerHTML = "";
+  //Clear board
+  DisplayController.clearBoard();
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    congrats.innerHTML = '';
-    //Clear board
-    DisplayController.clearBoard()
-    
-    //Set all value in array to empty
-    DisplayBoard.gameBoard=['','','','','','','','','']
-    GameFlow.changeGameStatus()
-    GameFlow.start();
-})
-
+  //Set all value in array to empty
+  DisplayBoard.gameBoard = ["", "", "", "", "", "", "", "", ""];
+  GameFlow.changeGameStatus();
+  GameFlow.start();
+});
