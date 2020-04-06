01606d9efc542dd9e5ed027dc855b07dea33700e
Edemone <edem.agbenyo@gmail.com>
2020-04-06 23:24:40 +0000

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "build")
+    contentBase: path.resolve(__dirname, 'build'),
   },
 };
