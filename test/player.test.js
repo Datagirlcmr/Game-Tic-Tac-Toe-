7eec3316ca8e262f00add95e861e6700472f9399
Selma Datagirl <datagirlcmr@gmail.com>
2020-04-06 15:57:40 -0500

    implemented the test for initializing a player

--- test/player.test.js
+++ test/player.test.js
@@ -1,5 +1,17 @@
+import Player from '../src/player';
+
 describe("Player Flow",()=>{
-  it("check game",()=>{
-    expect(true).toBe(true)
+  const player = new Player('Selma', 'X')
+
+  it("checks the player name",()=>{
+    expect(player.name).toBe('Selma')
+  })
+
+  it("checks the player symbol",()=>{
+    expect(player.symbol).toBe('X')
+  })
+
+  it("checks the player status",()=>{
+    expect(player.playing).toBe(false)
   })
 })
