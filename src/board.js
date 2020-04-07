677c643e21aa75a596d24eab5054ee2f24fe23d1
Edemone <edem.agbenyo@gmail.com>
2020-04-07 17:37:18 +0000

    Remove commented code

--- src/board.js
+++ src/board.js
@@ -3,18 +3,5 @@ class Board {
     this.gameBoard = ['', '', '', '', '', '', '', '', ''];
     this.boardCount = 0;
   }
-
-  // updateBoard(mark, position){
-  //   this.gameBoard[position] = mark;
-  //   this.boardCount++;
-  // }
-
-  // boardFull(){
-  //   if (this.boardCount == this.gameBoard.length) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
 }
 export default Board;
